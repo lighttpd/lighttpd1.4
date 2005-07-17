ec9e53fd5622f0131feda4f9c5981977f315d435
Jan Kneschke <jan@kneschke.de>
2005-07-17 08:27:44 +0000

    forgot to add it
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.3.x@463 152afb58-edef-0310-8abb-c4023f1b3aa9

---
+++ src/mod_cml_funcs.h
@@ -0,0 +1,15 @@
+#ifndef _MOD_CML_FUNCS_H_
+#define _MOD_CML_FUNCS_H_
+
+#include "config.h"
+
+#ifdef HAVE_LUA_H
+#include <lua.h>
+
+int f_crypto_md5(lua_State *L);
+int f_file_mtime(lua_State *L);
+int f_memcache_exists(lua_State *L);
+int f_memcache_get_string(lua_State *L);
+int f_memcache_get_long(lua_State *L);
+#endif
+#endif
