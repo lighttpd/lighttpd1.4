9dd464a72b9981b1ec5efae5ed04821995639ca9
Jan Kneschke <jan@kneschke.de>
2005-08-31 13:39:09 +0000

    moved last-modifed handling to response.c and let mod_cml_lua.c use it.
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-merge-1.4.x@655 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/response.h
+++ src/response.h
@@ -13,7 +13,7 @@ int response_header_overwrite(server *srv, connection *con, const char *key, siz
 
 handler_t http_response_prepare(server *srv, connection *con);
 int http_response_redirect_to_directory(server *srv, connection *con);
-int http_response_handle_cachable(server *srv, connection *con, time_t mtime);
+int http_response_handle_cachable(server *srv, connection *con, buffer * mtime);
 
 buffer * strftime_cache_get(server *srv, time_t last_mod);
 #endif
