74a71a2ff82134a9d5bb2f4c1b986cceff9d695b
Jan Kneschke <jan@kneschke.de>
2005-09-14 07:10:25 +0000

    exported splaytree_size
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-merge-1.4.x@700 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/splaytree.h
+++ src/splaytree.h
@@ -13,5 +13,12 @@ typedef struct tree_node {
 splay_tree * splaytree_splay (splay_tree *t, int key);
 splay_tree * splaytree_insert(splay_tree *t, int key, void *data);
 splay_tree * splaytree_delete(splay_tree *t, int key);
+splay_tree * splaytree_size(splay_tree *t);
+
+#define splaytree_size(x) (((x)==NULL) ? 0 : ((x)->size))
+/* This macro returns the size of a node.  Unlike "x->size",     */
+/* it works even if x=NULL.  The test could be avoided by using  */
+/* a special version of NULL which was a real node with size 0.  */
+ 
 
 #endif
