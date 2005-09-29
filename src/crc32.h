4d2eddef3503e9e12a7a17361419178af5ff0f7f
Jan Kneschke <jan@kneschke.de>
2005-09-29 13:02:50 +0000

    include either inttypes.h or stdint.h (fixes #291)
    git-svn-id: svn+ssh://svn.lighttpd.net/lighttpd/branches/lighttpd-merge-1.4.x@748 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/crc32.h
+++ src/crc32.h
@@ -1,8 +1,17 @@
 #ifndef __crc32cr_table_h__
 #define __crc32cr_table_h__
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <sys/types.h>
+
+#if defined HAVE_STDINT_H
 #include <stdint.h>
+#elif def HAVE_INTTYPES_H
+#include <inttypes.h>
+#endif
 
 uint32_t generate_crc32c(char *string, size_t length);
 
