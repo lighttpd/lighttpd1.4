5cdc27b0f7eb5d3f90d165c3387ddaba8cda9709
Elan Ruusamäe <glen@delfi.ee>
2008-01-21 08:21:20 +0000

    - support chained proxies in mod_extforward (#1528)
    git-svn-id: svn+ssh://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2061 152afb58-edef-0310-8abb-c4023f1b3aa9

--- tests/docroot/www/ip.pl
+++ tests/docroot/www/ip.pl
@@ -2,4 +2,12 @@
 print "Content-Type: text/html\r\n\r\n";
 print $ENV{'REMOTE_ADDR'};
 
+if ($ENV{'QUERY_STRING'} eq 'info') {
+	print "\nF:",$ENV{'HTTP_X_FORWARDED_FOR'},"\n";
+
+	while (my($key, $value) = each %ENV) {
+		printf "%s => %s\n", $key, $value;
+	}
+}
+
 0;
