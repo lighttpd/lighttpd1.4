19588f6ee6cacabcd5656317f53043efd920a9dc
Stefan Bühler <stbuehler@web.de>
2008-08-19 17:40:42 +0000

    Hide some ssl errors per default, enable them with debug.log-ssl-noise (#397)
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2291 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/base.h
+++ src/base.h
@@ -259,6 +259,7 @@ typedef struct {
 	unsigned short log_request_handling;
 	unsigned short log_response_header;
 	unsigned short log_condition_handling;
+	unsigned short log_ssl_noise;
 
 
 	/* server wide */
