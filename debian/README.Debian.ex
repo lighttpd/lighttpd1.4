1554d5c42b9909c87f274409f0d9e3e2d9a6442e
Stefan Bühler <stbuehler@web.de>
2008-09-30 21:13:42 +0000

    Removed debian/, openwrt/ and cygwin/; they weren't kept up-to-date, and we decided to remove dist. specific stuff
    git-svn-id: svn+ssh://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2320 152afb58-edef-0310-8abb-c4023f1b3aa9

--- debian/README.Debian.ex
+++
@@ -1,6 +0,0 @@
-lighttpd for Debian
--------------------
-
-<possible notes regarding this package - if none, delete this file>
-
- -- Vincent Wagelaar <vincent@hannibal.lr-s.tudelft.nl>, Wed, 24 Mar 2004 08:20:58 +0100
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lighttpd):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="lighttpd" command="/usr/bin/lighttpd"
