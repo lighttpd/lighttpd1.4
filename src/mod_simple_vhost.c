6ea62edd5264db9493a5030fcd9174f4d1e4ae56
Stefan Bühler <stbuehler@web.de>
2009-02-17 22:45:44 +0000

    Fix default vhost in mod_simple_vhost (fixes #1905)
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2399 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/mod_simple_vhost.c
+++ src/mod_simple_vhost.c
@@ -249,6 +249,8 @@ static handler_t mod_simple_vhost_docroot(server *srv, connection *con, void *p_
 				return HANDLER_GO_ON;
 			} else {
 				buffer_copy_string_buffer(con->server_name, p->conf.default_host);
+				buffer_copy_string_buffer(con->physical.doc_root, p->doc_root);
+
 				/* do not cache default host */
 				return HANDLER_GO_ON;
 			}
