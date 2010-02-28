34fb1258e4f6b3b714da7c41adcca6c08dd2ccec
Stefan Bühler <stbuehler@web.de>
2010-02-28 11:48:05 +0000

    Fix handling return value of SSL_CTX_set_options (fixes #2157, thx mlcreech)
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2716 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/network.c
+++ src/network.c
@@ -525,7 +525,7 @@ int network_init(server *srv) {
 
 		if (!s->ssl_use_sslv2) {
 			/* disable SSLv2 */
-			if (SSL_OP_NO_SSLv2 != SSL_CTX_set_options(s->ssl_ctx, SSL_OP_NO_SSLv2)) {
+			if (!(SSL_OP_NO_SSLv2 & SSL_CTX_set_options(s->ssl_ctx, SSL_OP_NO_SSLv2))) {
 				log_error_write(srv, __FILE__, __LINE__, "ss", "SSL:",
 						ERR_error_string(ERR_get_error(), NULL));
 				return -1;
