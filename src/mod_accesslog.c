cece2fe3c6701505110dd5a1767032e9af19d44e
Stefan Bühler <stbuehler@web.de>
2010-07-11 17:18:54 +0000

    mod_accesslog: Fix var declarations mixed in source (fixes #2233)
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2743 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/mod_accesslog.c
+++ src/mod_accesslog.c
@@ -157,12 +157,14 @@ INIT_FUNC(mod_accesslog_init) {
 }
 
 static void accesslog_append_escaped(buffer *dest, buffer *str) {
+	unsigned int i;
+
 	/* replaces non-printable chars with \xHH where HH is the hex representation of the byte */
 	/* exceptions: " => \", \ => \\, whitespace chars => \n \t etc. */
 	if (str->used == 0) return;
 	buffer_prepare_append(dest, str->used - 1);
 
-	for (unsigned int i = 0; i < str->used - 1; i++) {
+	for (i = 0; i < str->used - 1; i++) {
 		if (str->ptr[i] >= ' ' && str->ptr[i] <= '~') {
 			/* printable chars */
 			buffer_append_string_len(dest, &str->ptr[i], 1);
