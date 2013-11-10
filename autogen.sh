cb02c738602ea6d71fb19da83151174c771ca4d9
Stefan Bühler <stbuehler@web.de>
2013-11-10 13:41:09 +0000

    [autobuild] use autoreconf instead of calling tools manually; add generated files to .gitignore
    From: Stefan Bühler <stbuehler@web.de>
    
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2914 152afb58-edef-0310-8abb-c4023f1b3aa9

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-if which glibtoolize >/dev/null 2>&1; then
-  LIBTOOLIZE=${LIBTOOLIZE:-glibtoolize}
-else
-  LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
-fi
-ACLOCAL=${ACLOCAL:-aclocal}
-AUTOHEADER=${AUTOHEADER:-autoheader}
-AUTOMAKE=${AUTOMAKE:-automake}
-AUTOCONF=${AUTOCONF:-autoconf}
-
-ARGV0=$0
-
 set -e
 
-
-run() {
-  echo "$ARGV0: running \`$@'"
-  "$@"
-}
-
-run $LIBTOOLIZE --copy --force
-run $ACLOCAL $ACLOCAL_FLAGS
-run $AUTOHEADER
-run $AUTOMAKE --add-missing --copy --foreign --force-missing
-run $AUTOCONF
+autoreconf --force --install
 echo "Now type './configure ...' and 'make' to compile."
