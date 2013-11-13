18869bcfed660712252f037c3ee568a58f6e98cc
Stefan Bühler <stbuehler@web.de>
2013-11-13 17:18:35 +0000

    [mod_ssi] fix "ssi_val_init" prototype
    From: Stefan Bühler <stbuehler@web.de>
    
    git-svn-id: svn://svn.lighttpd.net/lighttpd/branches/lighttpd-1.4.x@2924 152afb58-edef-0310-8abb-c4023f1b3aa9

--- src/mod_ssi_expr.h
+++ src/mod_ssi_expr.h
@@ -25,7 +25,7 @@ void ssiexprparserFree(void *p, void (*freeProc)(void*));
 void ssiexprparser(void *yyp, int yymajor, buffer *yyminor, ssi_ctx_t *ctx);
 
 int ssi_val_tobool(ssi_val_t *B);
-ssi_val_t *ssi_val_init();
+ssi_val_t *ssi_val_init(void);
 void ssi_val_free(ssi_val_t *s);
 
 #endif
