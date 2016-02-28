5492063f356755e276344427d4ef8ad2541d3c8d
Glenn Strauss <gstrauss@gluelogic.com>
2016-02-28 12:05:22 -0500

    [core] set REDIRECT_STATUS to error_handler_saved_status (fixes #1828)
    set REDIRECT_STATUS to con->error_handler_saved_status in dynamic
    handlers for PHP compiled with --force-redirect.  Set to "200"
    if (0 == con->error_handler_saved_status)
    (mod_cgi, mod_fastcgi, mod_scgi, mod_ssi)
    
    FYI: setting REDIRECT_STATUS in con->environment allows access and
    manipulation by mod_magnet.
    
    x-ref:
      "REDIRECT_STATUS == 200 on 404 redirect"
      https://redmine.lighttpd.net/issues/1828
    
    github: closes #35

--- src/mod_cgi.c
+++ src/mod_cgi.c
@@ -1078,7 +1078,6 @@ static int cgi_create_env(server *srv, connection *con, plugin_data *p, handler_
 		if (!buffer_string_is_empty(con->request.pathinfo)) {
 			cgi_env_add(&env, CONST_STR_LEN("PATH_INFO"), CONST_BUF_LEN(con->request.pathinfo));
 		}
-		cgi_env_add(&env, CONST_STR_LEN("REDIRECT_STATUS"), CONST_STR_LEN("200"));
 		if (!buffer_string_is_empty(con->uri.query)) {
 			cgi_env_add(&env, CONST_STR_LEN("QUERY_STRING"), CONST_BUF_LEN(con->uri.query));
 		} else {
@@ -1086,6 +1085,11 @@ static int cgi_create_env(server *srv, connection *con, plugin_data *p, handler_
 		}
 		if (!buffer_string_is_empty(con->request.orig_uri)) {
 			cgi_env_add(&env, CONST_STR_LEN("REQUEST_URI"), CONST_BUF_LEN(con->request.orig_uri));
+		}
+		/* set REDIRECT_STATUS for php compiled with --force-redirect
+		 * (if REDIRECT_STATUS has not already been set by error handler) */
+		if (0 == con->error_handler_saved_status) {
+			cgi_env_add(&env, CONST_STR_LEN("REDIRECT_STATUS"), CONST_STR_LEN("200"));
 		}
 
 
