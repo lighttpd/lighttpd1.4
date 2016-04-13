f1681ca29bf1f945b59ff81437f02c4b48036017
Glenn Strauss <gstrauss@gluelogic.com>
2016-04-12 23:48:14 -0400

    [mod_cgi] always set QUERY_STRING (fixes #1339)
    (thx alexo)
    
    x-ref:
      "lighttpd doesn't set empty QUERY_STRING in cgi environment"
      https://redmine.lighttpd.net/issues/1339

--- src/mod_cgi.c
+++ src/mod_cgi.c
@@ -956,6 +956,8 @@ static int cgi_create_env(server *srv, connection *con, plugin_data *p, buffer *
 		cgi_env_add(&env, CONST_STR_LEN("REDIRECT_STATUS"), CONST_STR_LEN("200"));
 		if (!buffer_string_is_empty(con->uri.query)) {
 			cgi_env_add(&env, CONST_STR_LEN("QUERY_STRING"), CONST_BUF_LEN(con->uri.query));
+		} else {
+			cgi_env_add(&env, CONST_STR_LEN("QUERY_STRING"), CONST_STR_LEN(""));
 		}
 		if (!buffer_string_is_empty(con->request.orig_uri)) {
 			cgi_env_add(&env, CONST_STR_LEN("REQUEST_URI"), CONST_BUF_LEN(con->request.orig_uri));
