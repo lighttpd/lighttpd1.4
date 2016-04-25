b47c393e2629c40424a641b4219f71f7a1c23740
Glenn Strauss <gstrauss@gluelogic.com>
2016-04-25 04:35:02 -0400

    [mod_auth] skip blank lines and comment lines (fixes #2327)
    x-ref:
      "Ignore comments and empty lines in ht{digest,passwd} files"
      https://redmine.lighttpd.net/issues/2327

--- src/http_auth.c
+++ src/http_auth.c
@@ -86,6 +86,9 @@ static int http_auth_get_password(server *srv, mod_auth_plugin_data *p, buffer *
 			char *f_pwd, *f_realm;
 			size_t u_len, r_len;
 
+			/* skip blank lines and comment lines (beginning '#') */
+			if (f_user[0] == '#' || f_user[0] == '\n' || f_user[0] == '\0') continue;
+
 			/*
 			 * htdigest format
 			 *
@@ -152,6 +155,9 @@ static int http_auth_get_password(server *srv, mod_auth_plugin_data *p, buffer *
 		while (NULL != fgets(f_user, sizeof(f_user), fp)) {
 			char *f_pwd;
 			size_t u_len;
+
+			/* skip blank lines and comment lines (beginning '#') */
+			if (f_user[0] == '#' || f_user[0] == '\n' || f_user[0] == '\0') continue;
 
 			/*
 			 * htpasswd format
