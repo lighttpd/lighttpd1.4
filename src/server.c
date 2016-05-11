1cd31ae2ccf9e839325ccafdb72108e6b2d39745
Glenn Strauss <gstrauss@gluelogic.com>
2016-05-11 00:58:26 -0400

    adjustments for openssl 1.1.0 pre-release

--- src/server.c
+++ src/server.c
@@ -389,7 +389,8 @@ static void server_free(server *srv) {
 		ERR_free_strings();
 	      #if   OPENSSL_VERSION_NUMBER >= 0x10100000L \
 		&& !defined(LIBRESSL_VERSION_NUMBER)
-		ERR_remove_thread_state();
+		/*(OpenSSL libraries handle thread init and deinit)
+		 * https://github.com/openssl/openssl/pull/1048 */
 	      #elif OPENSSL_VERSION_NUMBER >= 0x10000000L
 		ERR_remove_thread_state(NULL);
 	      #else
