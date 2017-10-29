lighttpd/lighttpd1.4

commit 86bb8be2c8eb5a65ebf73f12f62833db84d2cf9c

[core] perf: skip redundant strlen() if len known
performance: skip redundant strlen() if length is already known

introduce array_get_element_klen() to take key and klen params

src/configfile.c
@@ -1347,7 +1347,7 @@ int config_read(server *srv, const char *fn) {
 	dcwd = data_string_init();
 	buffer_string_prepare_copy(dcwd->value, 1023);
 	if (NULL != getcwd(dcwd->value->ptr, dcwd->value->size - 1)) {
-		buffer_commit(dcwd->value, strlen(dcwd->value->ptr));
+		buffer_commit(dcwd->value, buffer_string_length(dcwd->value));
 		buffer_copy_string_len(dcwd->key, CONST_STR_LEN("var.CWD"));
 		array_insert_unique(dc->value, (data_unset *)dcwd);
 	} else {

src/array.c
@@ -120,11 +120,11 @@ static size_t array_get_index(array *a, const char *key, size_t keylen, size_t *
 	return ARRAY_NOT_FOUND;
 }
 
-data_unset *array_get_element(array *a, const char *key) {
+data_unset *array_get_element_klen(array *a, const char *key, size_t klen) {
 	size_t ndx;
 	force_assert(NULL != key);
 
-	if (ARRAY_NOT_FOUND != (ndx = array_get_index(a, key, strlen(key), NULL))) {
+	if (ARRAY_NOT_FOUND != (ndx = array_get_index(a, key, klen, NULL))) {
 		/* found, return it */
 		return a->data[ndx];
 	}
@@ -132,11 +132,11 @@ data_unset *array_get_element(array *a, const char *key) {
 	return NULL;
 }
 
-data_unset *array_extract_element(array *a, const char *key) {
+data_unset *array_extract_element_klen(array *a, const char *key, size_t klen) {
 	size_t ndx, pos;
 	force_assert(NULL != key);
 
-	if (ARRAY_NOT_FOUND != (ndx = array_get_index(a, key, strlen(key), &pos))) {
+	if (ARRAY_NOT_FOUND != (ndx = array_get_index(a, key, klen, &pos))) {
 		/* found */
 		const size_t last_ndx = a->used - 1;
 		data_unset *entry = a->data[ndx];
@@ -193,7 +193,7 @@ data_unset *array_get_unused_element(array *a, data_type_t t) {
 void array_set_key_value(array *hdrs, const char *key, size_t key_len, const char *value, size_t val_len) {
 	data_string *ds_dst;
 
-	if (NULL != (ds_dst = (data_string *)array_get_element(hdrs, key))) {
+	if (NULL != (ds_dst = (data_string *)array_get_element_klen(hdrs, key, key_len))) {
 		buffer_copy_string_len(ds_dst->value, value, val_len);
 		return;
 	}
@@ -330,7 +330,7 @@ size_t array_get_max_key_length(array *a) {
 	maxlen = 0;
 	for (i = 0; i < a->used; i ++) {
 		data_unset *du = a->data[i];
-		size_t len = strlen(du->key->ptr);
+		size_t len = buffer_string_length(du->key);
 
 		if (len > maxlen) {
 			maxlen = len;
@@ -388,7 +388,7 @@ int array_print(array *a, int depth) {
 				array_print_indent(depth + 1);
 			}
 			fprintf(stdout, "\"%s\"", du->key->ptr);
-			for (j = maxlen - strlen(du->key->ptr); j > 0; j --) {
+			for (j = maxlen - buffer_string_length(du->key); j > 0; j--) {
 				fprintf(stdout, " ");
 			}
 			fprintf(stdout, " => ");

src/array.h
@@ -163,8 +163,9 @@ int array_is_kvarray(array *a);
 int array_is_kvstring(array *a);
 int array_print(array *a, int depth);
 data_unset *array_get_unused_element(array *a, data_type_t t);
-data_unset *array_get_element(array *a, const char *key);
-data_unset *array_extract_element(array *a, const char *key); /* removes found entry from array */
+#define array_get_element(a, key) array_get_element_klen((a), (key), sizeof(key)-1)
+data_unset *array_get_element_klen(array *a, const char *key, size_t klen);
+data_unset *array_extract_element_klen(array *a, const char *key, size_t klen); /* removes found entry from array */
 void array_set_key_value(array *hdrs, const char *key, size_t key_len, const char *value, size_t val_len);
 void array_replace(array *a, data_unset *entry);
 int array_strcasecmp(const char *a, size_t a_len, const char *b, size_t b_len);

src/configfile-glue.c
@@ -35,7 +35,7 @@ int config_insert_values_internal(server *srv, array *ca, const config_values_t 
 
 	for (i = 0; cv[i].key; i++) {
 
-		if (NULL == (du = array_get_element(ca, cv[i].key))) {
+		if (NULL == (du = array_get_element_klen(ca, cv[i].key, strlen(cv[i].key)))) {
 			/* no found */
 
 			continue;
@@ -195,7 +195,7 @@ int config_insert_values_global(server *srv, array *ca, const config_values_t cv
 	for (i = 0; cv[i].key; i++) {
 		data_string *touched;
 
-		if (NULL == (du = array_get_element(ca, cv[i].key))) {
+		if (NULL == (du = array_get_element_klen(ca, cv[i].key, strlen(cv[i].key)))) {
 			/* no found */
 
 			continue;
@@ -491,7 +491,7 @@ static cond_result_t config_check_cond_nocache(server *srv, connection *con, dat
 
 	case COMP_HTTP_REQUEST_HEADER: {
 		data_string *ds;
-		if (NULL != (ds = (data_string *)array_get_element(con->request.headers, dc->comp_tag->ptr))) {
+		if (NULL != (ds = (data_string *)array_get_element_klen(con->request.headers, CONST_BUF_LEN(dc->comp_tag)))) {
 			l = ds->value;
 		} else {
 			l = srv->empty_string;

src/configparser.y
@@ -49,7 +49,7 @@ static data_unset *configparser_get_variable(config_t *ctx, const buffer *key) {
     fprintf(stderr, "get var on block: %s\n", dc->key->ptr);
     array_print(dc->value, 0);
 #endif
-    if (NULL != (du = array_get_element(dc->value, key->ptr))) {
+    if (NULL != (du = array_get_element_klen(dc->value, CONST_BUF_LEN(key)))) {
       return du->copy(du);
     }
   }
@@ -95,7 +95,7 @@ data_unset *configparser_merge_data(data_unset *op1, const data_unset *op2) {
       for (i = 0; i < src->used; i ++) {
         du = (data_unset *)src->data[i];
         if (du) {
-          if (du->is_index_key || buffer_is_empty(du->key) || !array_get_element(dst, du->key->ptr)) {
+          if (du->is_index_key || buffer_is_empty(du->key) || !array_get_element_klen(dst, CONST_BUF_LEN(du->key))) {
             array_insert_unique(dst, du->copy(du));
           } else {
             fprintf(stderr, "Duplicate array-key '%s'\n", du->key->ptr);
@@ -189,7 +189,7 @@ varline ::= key(A) ASSIGN expression(B). {
           ctx->current->context_ndx,
           ctx->current->key->ptr, A->ptr);
       ctx->ok = 0;
-    } else if (NULL == array_get_element(ctx->current->value, B->key->ptr)) {
+    } else if (NULL == array_get_element_klen(ctx->current->value, CONST_BUF_LEN(B->key))) {
       array_insert_unique(ctx->current->value, B);
       B = NULL;
     } else {
@@ -234,7 +234,7 @@ varline ::= key(A) APPEND expression(B). {
           ctx->current->context_ndx,
           ctx->current->key->ptr, A->ptr);
       ctx->ok = 0;
-    } else if (NULL != (du = array_extract_element(vars, A->ptr)) || NULL != (du = configparser_get_variable(ctx, A))) {
+    } else if (NULL != (du = array_extract_element_klen(vars, CONST_BUF_LEN(A))) || NULL != (du = configparser_get_variable(ctx, A))) {
       du = configparser_merge_data(du, B);
       if (NULL == du) {
         ctx->ok = 0;
@@ -351,7 +351,7 @@ aelements(A) ::= aelements(C) COMMA aelement(B). {
   A = NULL;
   if (ctx->ok) {
     if (buffer_is_empty(B->key) ||
-        NULL == array_get_element(C, B->key->ptr)) {
+        NULL == array_get_element_klen(C, CONST_BUF_LEN(B->key))) {
       array_insert_unique(C, B);
       B = NULL;
     } else {
@@ -408,7 +408,7 @@ eols ::= .
 
 globalstart ::= GLOBAL. {
   data_config *dc;
-  dc = (data_config *)array_get_element(ctx->srv->config_context, "global");
+  dc = (data_config *)array_get_element_klen(ctx->srv->config_context, CONST_STR_LEN("global"));
   force_assert(dc);
   configparser_push(ctx, dc, 0);
 }
@@ -453,7 +453,7 @@ condlines(A) ::= condlines(B) eols ELSE cond_else(C). {
   if (ctx->ok) {
     size_t pos;
     data_config *dc;
-    dc = (data_config *)array_extract_element(ctx->all_configs, C->key->ptr);
+    dc = (data_config *)array_extract_element_klen(ctx->all_configs, CONST_BUF_LEN(C->key));
     force_assert(C == dc);
     buffer_copy_buffer(C->key, B->key);
     /*buffer_copy_buffer(C->comp_key, B->comp_key);*/
@@ -480,7 +480,7 @@ condlines(A) ::= condlines(B) eols ELSE cond_else(C). {
       force_assert(0);
     }
 
-    if (NULL == (dc = (data_config *)array_get_element(ctx->all_configs, C->key->ptr))) {
+    if (NULL == (dc = (data_config *)array_get_element_klen(ctx->all_configs, CONST_BUF_LEN(C->key)))) {
       /* re-insert into ctx->all_configs with new C->key */
       array_insert_unique(ctx->all_configs, (data_unset *)C);
       C->prev = B;
@@ -568,7 +568,7 @@ context ::= DOLLAR SRVVARNAME(B) LBRACKET stringop(C) RBRACKET cond(E) expressio
     rvalue = ((data_string*)D)->value;
     buffer_append_string_buffer(b, rvalue);
 
-    if (NULL != (dc = (data_config *)array_get_element(ctx->all_configs, b->ptr))) {
+    if (NULL != (dc = (data_config *)array_get_element_klen(ctx->all_configs, CONST_BUF_LEN(b)))) {
       configparser_push(ctx, dc, 0);
     } else {
       static const struct {

src/data_config.c
@@ -81,7 +81,7 @@ static void data_config_print(const data_unset *d, int depth) {
 	maxlen = array_get_max_key_length(a);
 	for (i = 0; i < a->used; i ++) {
 		data_unset *du = a->data[i];
-		size_t len = strlen(du->key->ptr);
+		size_t len = buffer_string_length(du->key);
 		size_t j;
 
 		array_print_indent(depth);

src/http-header-glue.c
@@ -106,8 +106,8 @@ int response_header_overwrite(server *srv, connection *con, const char *key, siz
 	UNUSED(srv);
 
 	/* if there already is a key by this name overwrite the value */
-	if (NULL != (ds = (data_string *)array_get_element(con->response.headers, key))) {
-		buffer_copy_string(ds->value, value);
+	if (NULL != (ds = (data_string *)array_get_element_klen(con->response.headers, key, keylen))) {
+		buffer_copy_string_len(ds->value, value, vallen);
 
 		return 0;
 	}
@@ -121,7 +121,7 @@ int response_header_append(server *srv, connection *con, const char *key, size_t
 	UNUSED(srv);
 
 	/* if there already is a key by this name append the value */
-	if (NULL != (ds = (data_string *)array_get_element(con->response.headers, key))) {
+	if (NULL != (ds = (data_string *)array_get_element_klen(con->response.headers, key, keylen))) {
 		buffer_append_string_len(ds->value, CONST_STR_LEN(", "));
 		buffer_append_string_len(ds->value, value, vallen);
 		return 0;

src/mod_accesslog.c
@@ -927,14 +927,14 @@ REQUESTDONE_FUNC(log_access_write) {
 				}
 				break;
 			case FORMAT_HEADER:
-				if (NULL != (ds = (data_string *)array_get_element(con->request.headers, f->string->ptr))) {
+				if (NULL != (ds = (data_string *)array_get_element_klen(con->request.headers, CONST_BUF_LEN(f->string)))) {
 					accesslog_append_escaped(b, ds->value);
 				} else {
 					buffer_append_string_len(b, CONST_STR_LEN("-"));
 				}
 				break;
 			case FORMAT_RESPONSE_HEADER:
-				if (NULL != (ds = (data_string *)array_get_element(con->response.headers, f->string->ptr))) {
+				if (NULL != (ds = (data_string *)array_get_element_klen(con->response.headers, CONST_BUF_LEN(f->string)))) {
 					accesslog_append_escaped(b, ds->value);
 				} else {
 					buffer_append_string_len(b, CONST_STR_LEN("-"));
@@ -942,7 +942,7 @@ REQUESTDONE_FUNC(log_access_write) {
 				break;
 			case FORMAT_ENV:
 			case FORMAT_NOTE:
-				if (NULL != (ds = (data_string *)array_get_element(con->environment, f->string->ptr))) {
+				if (NULL != (ds = (data_string *)array_get_element_klen(con->environment, CONST_BUF_LEN(f->string)))) {
 					accesslog_append_escaped(b, ds->value);
 				} else {
 					buffer_append_string_len(b, CONST_STR_LEN("-"));

commit b27f1c0910379e8be7e23818d2dbdeb650a7d743

[core] fix var.CWD (regression in 1.4.46) (fixes #2835)
(broken in commit:86bb8be2)

x-ref:
  "empty var.CWD"
  https://redmine.lighttpd.net/issues/2835

src/configfile.c
@@ -1355,7 +1355,7 @@ int config_read(server *srv, const char *fn) {
 	dcwd = data_string_init();
 	buffer_string_prepare_copy(dcwd->value, 1023);
 	if (NULL != getcwd(dcwd->value->ptr, dcwd->value->size - 1)) {
-		buffer_commit(dcwd->value, buffer_string_length(dcwd->value));
+		buffer_commit(dcwd->value, strlen(dcwd->value->ptr));
 		buffer_copy_string_len(dcwd->key, CONST_STR_LEN("var.CWD"));
 		array_insert_unique(dc->value, (data_unset *)dcwd);
 	} else {