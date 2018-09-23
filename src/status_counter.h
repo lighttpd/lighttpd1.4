2dbcfc926604df95bb8ba7d6a8cfee96a2fcfefd
Glenn Strauss <gstrauss@gluelogic.com>
2018-09-23 00:06:07 -0400

    [core] inline status_counter routines

--- src/status_counter.h
+++ src/status_counter.h
@@ -4,9 +4,39 @@
 
 #include "base_decls.h"
 
+static inline
 int *status_counter_get_counter(server *srv, const char *s, size_t len);
+static inline
 void status_counter_inc(server *srv, const char *s, size_t len);
+static inline
 void status_counter_dec(server *srv, const char *s, size_t len);
+static inline
 void status_counter_set(server *srv, const char *s, size_t len, int val);
+
+/* inline status counter routines */
+
+#include "base.h"       /* (srv->status) */
+#include "array.h"
+
+static inline
+int *status_counter_get_counter(server *srv, const char *s, size_t len) {
+    return array_get_int_ptr(srv->status, s, len);
+}
+
+static inline
+void status_counter_inc(server *srv, const char *s, size_t len) {
+    ++(*array_get_int_ptr(srv->status, s, len));
+}
+
+static inline
+void status_counter_dec(server *srv, const char *s, size_t len) {
+    --(*array_get_int_ptr(srv->status, s, len));
+}
+
+static inline
+void status_counter_set(server *srv, const char *s, size_t len, int val) {
+    *array_get_int_ptr(srv->status, s, len) = val;
+}
+
 
 #endif
