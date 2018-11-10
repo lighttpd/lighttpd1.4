bc91bbd0c6ee4801f11c83337ff93a1e67250c90
Rosen Penev <rosenp@gmail.com>
2018-11-09 16:29:20 -0800

    [core] Don't call RAND_cleanup with OpenSSL 1.1.x
    RAND_cleanup is deprecated and does nothing with 1.1.x.
    
    It also breaks with OpenSSL compiled with no deprecated APIs.
    (-DOPENSSL_API_COMPAT=<version>)
    
    github: closes #93

--- src/rand.c
+++ src/rand.c
@@ -224,7 +224,9 @@ int li_rand_bytes (unsigned char *buf, int num)
 void li_rand_cleanup (void)
 {
   #ifdef USE_OPENSSL_CRYPTO
+  #if OPENSSL_VERSION_NUMBER < 0x10100000L
     RAND_cleanup();
+  #endif
   #endif
     safe_memclear(xsubi, sizeof(xsubi));
 }
