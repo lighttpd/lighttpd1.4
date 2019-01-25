lighttpd/lighttpd1.4

commit a46bc4f5de49c742e4bdc6ac7be6a4e567c8d92a

[core] remove proc_open.[ch], reduce stdio.h use

src/configfile.c
@@ -7,7 +7,6 @@
 
 #include "configparser.h"
 #include "configfile.h"
-#include "proc_open.h"
 #include "request.h"
 #include "stat_cache.h"
 
@@ -1103,9 +1102,7 @@ static int config_tokenizer(server *srv, tokenizer_t *t, int *token_id, buffer *
 
 		return 1;
 	} else if (t->offset < t->size) {
-		fprintf(stderr, "%s.%d: %d, %s\n",
-			__FILE__, __LINE__,
-			tid, token->ptr);
+		log_error_write(srv, __FILE__, __LINE__, "Dsb", tid, ",", token);
 	}
 	return 0;
 }
@@ -1261,9 +1258,10 @@ static char* getCWD(void) {
 
 int config_parse_cmd(server *srv, config_t *context, const char *cmd) {
 	tokenizer_t t;
-	int ret;
+	int ret = 0;
+	FILE *fp;
 	buffer *source;
-	buffer *out;
+	buffer *out = srv->tmp_buf;
 	char *oldpwd;
 
 	if (NULL == (oldpwd = getCWD())) {
@@ -1282,19 +1280,38 @@ int config_parse_cmd(server *srv, config_t *context, const char *cmd) {
 	}
 
 	source = buffer_init_string(cmd);
-	out = buffer_init();
 
-	if (0 != proc_open_buffer(cmd, NULL, out, NULL)) {
-		log_error_write(srv, __FILE__, __LINE__, "sbss",
-			"opening", source, "failed:", strerror(errno));
+	fp = popen(cmd, "r");
+	if (NULL == fp) {
+		log_error_write(srv, __FILE__, __LINE__, "SSss",
+				"popen \"", cmd, "\"failed:", strerror(errno));
 		ret = -1;
-	} else {
+	}
+	else {
+		size_t rd;
+		buffer_string_set_length(out, 0);
+		do {
+			rd = fread(buffer_string_prepare_append(out, 1023), 1, 1023, fp);
+			buffer_commit(out, rd);
+		} while (0 != rd && !ferror(fp));
+		if (0 != rd || !feof(fp)) {
+			log_error_write(srv, __FILE__, __LINE__, "SSss",
+					"fread \"", cmd, "\"failed:", strerror(errno));
+			ret = -1;
+		}
+		if (0 != pclose(fp)) {
+			log_error_write(srv, __FILE__, __LINE__, "SSss",
+					"pclose \"", cmd, "\"failed:", strerror(errno));
+			ret = -1;
+		}
+	}
+
+	if (-1 != ret) {
 		tokenizer_init(&t, source, CONST_BUF_LEN(out));
 		ret = config_parse(srv, context, &t);
 	}
 
 	buffer_free(source);
-	buffer_free(out);
 	if (0 != chdir(oldpwd)) {
 		log_error_write(srv, __FILE__, __LINE__, "sss",
 			"cannot change directory to", oldpwd, strerror(errno));

.gitignore
@@ -43,7 +43,6 @@ m4/
 missing
 mod_ssi_exprparser.c
 mod_ssi_exprparser.h
-proc_open
 scgi-responder
 sconsbuild/
 stamp-h1

src/CMakeLists.txt
@@ -604,7 +604,6 @@ add_executable(lighttpd
 	configfile.c
 	configparser.c
 	request.c
-	proc_open.c
 	${COMMON_SRC}
 )
 set(L_INSTALL_TARGETS ${L_INSTALL_TARGETS} lighttpd)

src/Makefile.am
@@ -1,6 +1,6 @@
 AM_CFLAGS = $(FAM_CFLAGS) $(LIBUNWIND_CFLAGS)
 
-noinst_PROGRAMS=proc_open test_buffer test_base64 test_configfile test_request
+noinst_PROGRAMS=test_buffer test_base64 test_configfile test_request
 sbin_PROGRAMS=lighttpd lighttpd-angel
 LEMON=$(top_builddir)/src/lemon$(BUILD_EXEEXT)
 
@@ -84,7 +84,7 @@ src = server.c response.c connections.c \
 	inet_ntop_cache.c \
 	network.c \
 	network_write.c \
-	configfile.c configparser.c proc_open.c
+	configfile.c configparser.c
 
 lib_LTLIBRARIES =
 
@@ -397,7 +397,7 @@ hdr = server.h base64.h buffer.h network.h log.h http_kv.h keyvalue.h \
 	rand.h \
 	sys-endian.h sys-mmap.h sys-socket.h sys-strings.h \
 	mod_cml.h mod_cml_funcs.h \
-	safe_memclear.h sock_addr.h splaytree.h proc_open.h status_counter.h \
+	safe_memclear.h sock_addr.h splaytree.h status_counter.h \
 	mod_magnet_cache.h
 
 
@@ -510,10 +510,6 @@ lighttpd_LDADD = $(PCRE_LIB) $(DL_LIB) $(SENDFILE_LIB) $(ATTR_LIB) $(common_liba
 lighttpd_LDFLAGS = -export-dynamic
 
 endif
-
-proc_open_SOURCES = proc_open.c buffer.c
-proc_open_LDADD = $(LIBUNWIND_LIBS)
-proc_open_CPPFLAGS= -DDEBUG_PROC_OPEN
 
 test_buffer_SOURCES = test_buffer.c buffer.c
 test_buffer_LDADD = $(LIBUNWIND_LIBS)

src/SConscript
@@ -83,7 +83,7 @@ src = Split("server.c response.c connections.c \
 	inet_ntop_cache.c \
 	network.c \
 	network_write.c \
-	configfile.c configparser.c request.c proc_open.c")
+	configfile.c configparser.c request.c")
 
 lemon = env.Program('lemon', 'lemon.c', LIBS = GatherLibs(env))
 

src/array.c
@@ -4,7 +4,6 @@
 #include "buffer.h"
 
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
@@ -316,6 +315,10 @@ int array_is_kvstring(array *a) {
 	}
 	return 1;
 }
+
+
+
+#include <stdio.h>
 
 void array_print_indent(int depth) {
 	int i;

src/data_array.c
@@ -3,7 +3,6 @@
 #include "array.h"
 
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 static data_unset *data_array_copy(const data_unset *s) {

src/meson.build
@@ -585,7 +585,6 @@ main_src = [
 	'inet_ntop_cache.c',
 	'network_write.c',
 	'network.c',
-	'proc_open.c',
 	'request.c',
 	'response.c',
 	'server.c',

commit 513aa1d3c4ba6b7198cc5f3c39f5fd72b10fc41c

[core] Fix recursive include_shell invocations
When the output of include_shell calls include_shell itself,
that second invocation must not truncate the buffer used for the
outer include_shell.

This might sound like a pathological setup in itself, but with
e.g.  debian's include-conf-enabled.pl, which outputs a list of
include statements for all files in /etc/lighttpd/conf-enabled,
if any of the *.conf files in that directory invokes include_shell,
the parsing of the rest of the files in that directory is effectively
aborted.

This fixes a regression since commit
a46bc4f5de49c742e4bdc6ac7be6a4e567c8d92a in such setups.

src/configfile.c
@@ -1437,8 +1437,7 @@ int config_parse_cmd(server *srv, config_t *context, const char *cmd) {
 			ssize_t rd;
 			pid_t wpid;
 			int wstatus;
-			buffer *out = srv->tmp_buf;
-			buffer_clear(out);
+			buffer *out = buffer_init();
 			close(fds[1]);
 			fds[1] = -1;
 			do {
@@ -1471,6 +1470,7 @@ int config_parse_cmd(server *srv, config_t *context, const char *cmd) {
 				ret = config_parse(srv, context, &t);
 				buffer_free(source);
 			}
+			buffer_free(out);
 		}
 		if (-1 != fds[0]) close(fds[0]);
 		if (-1 != fds[1]) close(fds[1]);