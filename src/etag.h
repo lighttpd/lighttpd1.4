c7382615024cf73453e6f4c3319513ce2b3f4071
Glenn Strauss <gstrauss@gluelogic.com>
2019-03-17 17:25:07 -0400

    [core] add const to some etag prototypes

--- src/etag.h
+++ src/etag.h
@@ -8,8 +8,8 @@ struct stat;            /* declaration */
 
 typedef enum { ETAG_USE_INODE = 1, ETAG_USE_MTIME = 2, ETAG_USE_SIZE = 4 } etag_flags_t;
 
-int etag_is_equal(buffer *etag, const char *matches, int weak_ok);
-int etag_create(buffer *etag, struct stat *st, etag_flags_t flags);
+int etag_is_equal(const buffer *etag, const char *matches, int weak_ok);
+int etag_create(buffer *etag, const struct stat *st, etag_flags_t flags);
 int etag_mutate(buffer *mut, buffer *etag);
 
 
