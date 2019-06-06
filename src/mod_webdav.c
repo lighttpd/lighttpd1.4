075241c773931d8ed9f2cc7d60a28ce95c48d497
Glenn Strauss <gstrauss@gluelogic.com>
2019-06-05 23:54:44 -0400

    [mod_webdav] fix startup crash w/ multiple conds (fixes #2958)
    (thx flynn)
    
    x-ref:
      "lighttpd 1.4.54 segfaults on start in mod_webdav"
      https://redmine.lighttpd.net/issues/2958

--- src/mod_webdav.c
+++ src/mod_webdav.c
@@ -1275,8 +1275,8 @@ SERVER_FUNC(mod_webdav_worker_init)
      *   across a fork() system call into the child process.
      */
     plugin_data * const p = (plugin_data *)p_d;
-    plugin_config *s = p->config_storage[0];
-    for (int n_context = p->nconfig+1; --n_context; ++s) {
+    for (int i = 0; i < p->nconfig; ++i) {
+        plugin_config *s = p->config_storage[i];
         if (!buffer_is_empty(s->sqlite_db_name)
             && mod_webdav_sqlite3_prep(s->sql, s->sqlite_db_name, srv->errh)
                == HANDLER_ERROR)
