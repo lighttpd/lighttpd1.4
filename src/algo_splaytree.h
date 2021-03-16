a83b5a012155ab4b9dfc724aa5b551c0bc60cc5c
Glenn Strauss <gstrauss@gluelogic.com>
2021-03-16 19:44:46 -0400

    [core] remove bad prototype from algo_splaytree.h

--- src/algo_splaytree.h
+++ src/algo_splaytree.h
@@ -14,7 +14,6 @@ typedef struct tree_node {
 splay_tree * splaytree_splay (splay_tree *t, int key);
 splay_tree * splaytree_insert(splay_tree *t, int key, void *data);
 splay_tree * splaytree_delete(splay_tree *t, int key);
-splay_tree * splaytree_size(splay_tree *t);
 
 #define splaytree_size(x) (((x)==NULL) ? 0 : ((x)->size))
 /* This macro returns the size of a node.  Unlike "x->size",     */
