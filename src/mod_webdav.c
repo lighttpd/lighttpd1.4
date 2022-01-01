5567fb64953637b93af2b0f414e3fd3a4dc319e7
Glenn Strauss <gstrauss@gluelogic.com>
2022-01-01 18:32:37 -0500

    [mod_webdav] copy_file_range() new in FreeBSD 13
    (thx devnexen)
    
    adjust feature defines for header visibility of copy_file_range()
    (introduced in FreeBSD 13)
    
    x-ref:
      https://redmine.lighttpd.net/issues/3128
      https://github.com/freebsd/freebsd-src/blob/main/sys/sys/cdefs.h#L634

--- src/mod_webdav.c
+++ src/mod_webdav.c
@@ -169,6 +169,13 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
+#ifdef HAVE_COPY_FILE_RANGE /* FreeBSD 13+ */
+#ifdef __FreeBSD__ /* FreeBSD strict compliance hides libc extensions */
+/*#define _XOPEN_SOURCE 700*//*(defined above)*/
+#define _ISOC11_SOURCE
+#define __BSD_VISIBLE 1
+#endif
+#endif
 
 #include "first.h"      /* first */
 #include "sys-mmap.h"
