8b38a6eb351d6ecb9c47b921005728f988d67e9c
Glenn Strauss <gstrauss@gluelogic.com>
2022-01-03 20:06:46 -0500

    [core] errno_t and rsize_t detection on Illumos
    (thx devnexen)

--- src/ck.h
+++ src/ck.h
@@ -18,14 +18,11 @@
 #endif
 #endif
 #include "first.h"
-#ifdef __FreeBSD__
+#if defined(__FreeBSD__) || defined(__sun)
 #ifndef _RSIZE_T_DEFINED /* expecting __EXT1_VISIBLE 1 and _RSIZE_T_DEFINED */
 #define _RSIZE_T_DEFINED
 typedef size_t rsize_t;
 #endif
-#include <errno.h>
-#endif
-#ifdef __sun
 #include <errno.h>
 #endif
 
