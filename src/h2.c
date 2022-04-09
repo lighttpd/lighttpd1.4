0f3075b8851e1fef32af9d6939e836a29b55e469
Glenn Strauss <gstrauss@gluelogic.com>
2022-04-09 14:33:16 -0400

    [core] send HTTP/2 SETTINGS_NO_RFC7540_PRIORITIES
    send HTTP/2 SETTINGS_NO_RFC7540_PRIORITIES=1 with server connection
    preface
    
    RFC9218 Extensible Prioritization Scheme for HTTP
      2.1 Disabling RFC 7540 Priorities
      If endpoints use SETTINGS_NO_RFC7540_PRIORITIES they MUST send it in
      the first SETTINGS frame.
    
    RFC7540 Hypertext Transfer Protocol Version 2 (HTTP/2)
      6.5.3.  Settings Synchronization
      Unsupported parameters MUST be ignored.
    
    x-ref:
      https://datatracker.ietf.org/doc/html/rfc9218
      https://datatracker.ietf.org/doc/html/rfc7540

--- src/h2.c
+++ src/h2.c
@@ -1931,7 +1931,7 @@ h2_init_con (request_st * const restrict h2r, connection * const restrict con, c
 
     static const uint8_t h2settings[] = { /*(big-endian numbers)*/
       /* SETTINGS */
-      0x00, 0x00, 0x12        /* frame length */ /* 3 * (6 bytes per setting) */
+      0x00, 0x00, 0x18        /* frame length */ /* 4 * (6 bytes per setting) */
      ,H2_FTYPE_SETTINGS       /* frame type */
      ,0x00                    /* frame flags */
      ,0x00, 0x00, 0x00, 0x00  /* stream identifier */
@@ -1960,13 +1960,8 @@ h2_init_con (request_st * const restrict h2r, connection * const restrict con, c
      ,0x00, 0x00, 0xFF, 0xFF  /* 65535 */
      ,0x00, H2_SETTINGS_ENABLE_CONNECT_PROTOCOL
      ,0x00, 0x00, 0x00, 0x01  /* 1 */
-
-     #if 0  /* omit sending unconditionally;
-             * some clients might not handle unrecognized setting */
-            /* (use of PRIORITY will probably die out on its own) */
      ,0x00, H2_SETTINGS_NO_RFC7540_PRIORITIES
      ,0x00, 0x00, 0x00, 0x01  /* 1 */
-     #endif
 
      #if 0
       /* WINDOW_UPDATE */
