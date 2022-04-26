ddb7f5640d958f07cb51f7ee97059a8ebe539e0d
Glenn Strauss <gstrauss@gluelogic.com>
2022-04-26 00:36:22 -0400

    [core] remove func decls duplicated in plugin.h

--- src/plugin.h
+++ src/plugin.h
@@ -3,8 +3,6 @@
 #include "first.h"
 
 #include "base_decls.h"
-#include "buffer.h"
-#include "array.h"
 #include "plugin_config.h"
 
 
@@ -75,39 +73,5 @@ struct plugin {
 	size_t version;
 	void *lib;       /* dlopen handle */
 };
-
-__attribute_cold__
-int plugins_load(server *srv);
-
-__attribute_cold__
-void plugins_free(server *srv);
-
-handler_t plugins_call_handle_uri_clean(request_st *r);
-handler_t plugins_call_handle_subrequest_start(request_st *r);
-handler_t plugins_call_handle_response_start(request_st *r);
-handler_t plugins_call_handle_request_env(request_st *r);
-handler_t plugins_call_handle_request_done(request_st *r);
-handler_t plugins_call_handle_docroot(request_st *r);
-handler_t plugins_call_handle_physical(request_st *r);
-handler_t plugins_call_handle_request_reset(request_st *r);
-
-handler_t plugins_call_handle_connection_accept(connection *con);
-handler_t plugins_call_handle_connection_shut_wr(connection *con);
-handler_t plugins_call_handle_connection_close(connection *con);
-
-void plugins_call_handle_trigger(server *srv);
-handler_t plugins_call_handle_waitpid(server *srv, pid_t pid, int status);
-
-__attribute_cold__
-void plugins_call_handle_sighup(server *srv);
-
-__attribute_cold__
-handler_t plugins_call_init(server *srv);
-
-__attribute_cold__
-handler_t plugins_call_set_defaults(server *srv);
-
-__attribute_cold__
-handler_t plugins_call_worker_init(server *srv);
 
 #endif
