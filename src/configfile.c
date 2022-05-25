a6d4a98c07f68aa8a41965daffd2cd78fb7737fd
Glenn Strauss <gstrauss@gluelogic.com>
2022-05-25 13:29:27 -0400

    [core] use current dir if context->basedir blank

--- src/configfile.c
+++ src/configfile.c
@@ -2169,7 +2169,8 @@ int config_parse_file(server *srv, config_t *context, const char *fn) {
       #endif
 	glob_t gl;
 
-	if ((fn[0] == '/' || fn[0] == '\\') ||
+	if (buffer_is_blank(context->basedir) ||
+	    (fn[0] == '/' || fn[0] == '\\') ||
 	    (fn[0] == '.' && (fn[1] == '/' || fn[1] == '\\')) ||
 	    (fn[0] == '.' && fn[1] == '.' && (fn[2] == '/' || fn[2] == '\\'))) {
 		buffer_copy_string_len(filename, fn, fnlen);
