e1e713c5336f4241c61cf754b66ffac51b2f6535
Glenn Strauss <gstrauss@gluelogic.com>
2022-12-20 21:45:00 -0500

    [lighttpd-angel] simplify
    improve portability to ancient systems
    e.g. use signal() instead of sigaction()

--- src/lighttpd-angel.c
+++ src/lighttpd-angel.c
@@ -1,7 +1,7 @@
 #include "first.h"
 
 /**
- * angel process for lighttpd 
+ * angel process for lighttpd
  *
  * the purpose is the run as root all the time and handle:
  * - restart on crash
@@ -14,145 +14,93 @@
 #include <sys/wait.h>
 
 #include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <signal.h>
+#include <unistd.h>     /* _exit() execvp() fork() */
 
 #define BINPATH SBIN_DIR"/lighttpd"
 
-static siginfo_t last_sigterm_info;
-static siginfo_t last_sighup_info;
+static volatile sig_atomic_t restart = 0;
+static pid_t pid = -1;
 
-static volatile sig_atomic_t start_process    = 1;
-static volatile pid_t pid = -1;
+__attribute_cold__
+static void signal_handler (int sig)
+{
+    if (pid <= 0) return;
 
-#define UNUSED(x) ( (void)(x) )
+    if (sig == SIGHUP) {
+        /* trigger graceful shutdown of lighttpd, then restart lighttpd */
+        sig = SIGINT;
+        restart = -1;
+    }
 
-static void sigaction_handler(int sig, siginfo_t *si, void *context) {
-	UNUSED(context);
-	switch (sig) {
-	case SIGINT: 
-	case SIGTERM:
-	case SIGUSR1:
-		if (pid <= 0) break;
-		memcpy(&last_sigterm_info, si, sizeof(*si));
-
-		/** forward the sig to the child */
-		kill(pid, sig);
-		break;
-	case SIGHUP: /** do a graceful restart */
-		if (pid <= 0) break;
-		memcpy(&last_sighup_info, si, sizeof(*si));
-
-		/** do a graceful shutdown on the main process and start a new child */
-		kill(pid, SIGINT);
-		start_process = -1;/*(flag to waitpid(), then restart process)*/
-		break;
-	case SIGCHLD:
-		/* defer handling to waitpid() in main() */
-		break;
-	}
+    /* forward signal to the child */
+    kill(pid, sig);
 }
 
-int main(int argc, char **argv) {
-	int is_shutdown = 0;
-	struct sigaction act;
+__attribute_cold__
+static void signal_setup (void)
+{
+    signal(SIGCHLD, SIG_DFL);
+    signal(SIGALRM, SIG_DFL);
 
-	UNUSED(argc);
-	*(const char **)&argv[0] = BINPATH;
-      #ifdef __COVERITY__
-	__coverity_tainted_data_sanitize__(argv);
-      #endif
+    signal(SIGPIPE, SIG_IGN);
 
-	/**
-	 * we are running as root BEWARE
-	 */
-
-	memset(&act, 0, sizeof(act));
-	act.sa_handler = SIG_IGN;
-	sigaction(SIGPIPE, &act, NULL);
-	sigaction(SIGUSR1, &act, NULL);
-
-	act.sa_sigaction = sigaction_handler;
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_SIGINFO;
-
-	sigaction(SIGINT,  &act, NULL);
-	sigaction(SIGTERM, &act, NULL);
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGHUP,  &act, NULL);
-	sigaction(SIGALRM, &act, NULL);
-	sigaction(SIGCHLD, &act, NULL);
-
-	/* check that the compiled in path has the right user,
-	 *
-	 * BEWARE: there is a race between the check here and the exec later
-	 */
-
-	while (!is_shutdown) {
-		int exitcode = 0;
-
-		if (start_process == 1) {
-			pid = fork();
-
-			if (0 == pid) {
-				/* i'm the child */
-
-				/* intentionally pass argv params */
-				/* coverity[tainted_string : FALSE] */
-				execvp(argv[0], argv);
-				_exit(1);
-			} else if (-1 == pid) {
-				/** error */
-
-				return -1;
-			}
-
-			/* I'm the angel */
-			start_process = 0;
-		}
-	       
-		if ((pid_t)-1 == waitpid(pid, &exitcode, 0)) {
-			switch (errno) {
-			case EINTR:
-				/* someone sent a signal ... 
-				 * do we have to shutdown or restart the process */
-				break;
-			case ECHILD:
-				/** 
-				 * make sure we are not in a race between the signal handler
-				 * and the process restart */
-				if (!start_process) is_shutdown = 1;
-				break;
-			default:
-				break;
-			}
-		} else {
-			/** process went away */
-
-			if (WIFEXITED(exitcode)) {
-				/** normal exit */
-
-				is_shutdown = 1;
-
-				fprintf(stderr, "%s.%d: child (pid=%d) exited normally with exitcode: %d\n", 
-						__FILE__, __LINE__,
-						pid,
-						WEXITSTATUS(exitcode));
-
-			} else if (WIFSIGNALED(exitcode)) {
-				/** got a signal */
-
-				fprintf(stderr, "%s.%d: child (pid=%d) exited unexpectedly with signal %d, restarting\n", 
-						__FILE__, __LINE__,
-						pid,
-						WTERMSIG(exitcode));
-
-				start_process = 1;
-			}
-		}
-	}
-
-	return 0;
+    signal(SIGINT,  signal_handler);
+    signal(SIGTERM, signal_handler);
+    signal(SIGUSR1, signal_handler);
+    signal(SIGHUP,  signal_handler);
 }
 
+__attribute_cold__
+int main (int argc, char **argv)
+{
+    UNUSED(argc);
+    *(const char **)&argv[0] = BINPATH;
+  #ifdef __COVERITY__
+    __coverity_tainted_data_sanitize__(argv);
+  #endif
+
+    signal_setup();
+
+    do {
+
+        if (-1 == pid) {
+            pid = fork();
+            if (-1 == pid) return -1;
+            if (0 == pid) {
+                /* intentionally pass argv params */
+                /* coverity[tainted_string : FALSE] */
+                execvp(argv[0], argv);
+                _exit(1);
+            }
+        }
+
+        int exitcode = 0;
+        if ((pid_t)-1 == waitpid(pid, &exitcode, 0)) {
+            if (errno == ECHILD) break; /* child exists; should not happen */
+            continue;
+        }
+
+        const char *msg = NULL;
+        int code = 0;
+        if (WIFEXITED(exitcode)) {
+            code = WEXITSTATUS(exitcode);
+            msg = "%s.%d: child (pid=%d) exited normally with exitcode: %d\n";
+        }
+        else if (WIFSIGNALED(exitcode)) {
+            code = WTERMSIG(exitcode);
+            msg = "%s.%d: child (pid=%d) exited unexpectedly with signal %d, "
+                  "restarting\n";
+            restart = -1;
+        }
+        if (msg)
+            fprintf(stderr, msg, __FILE__, __LINE__, pid, code);
+
+        pid = restart; /* -1 for restart, 0 to exit */
+        restart = 0;
+
+    } while (pid != 0);
+
+    return 0;
+}
