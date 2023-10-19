546a4f76d40bd8f68e567de824bac4f64f0dcbd0
Glenn Strauss <gstrauss@gluelogic.com>
2023-10-19 07:36:55 -0400

    [core] omit dlopen trace for mod_h2, mod_deflate
    omit dlopen trace for mod_h2, mod_deflate if modules are missing
    and were auto-loaded

--- src/plugin.c
+++ src/plugin.c
@@ -131,7 +131,6 @@ int plugins_load(server *srv) {
 			}
 		}
 		if (!load_functions[j].name) {
-			log_error(srv->errh, __FILE__, __LINE__, "%s plugin not found", module);
 			if (srv->srvconf.compat_module_load) {
 				if (buffer_eq_slen(&ds->value, CONST_STR_LEN("mod_deflate")))
 					continue;
@@ -140,6 +139,7 @@ int plugins_load(server *srv) {
 				srv->srvconf.h2proto = 0;
 				continue;
 			}
+			log_error(srv->errh, __FILE__, __LINE__, "%s plugin not found", module);
 			return -1;
 		}
 	}
@@ -182,8 +182,6 @@ int plugins_load(server *srv) {
 	  #ifdef _WIN32
 		buffer_append_string_len(tb, CONST_STR_LEN(".dll"));
 		if (NULL == (lib = LoadLibrary(tb->ptr))) {
-			log_perror(srv->errh, __FILE__, __LINE__,
-			  "LoadLibrary() %s", tb->ptr);
 			if (srv->srvconf.compat_module_load) {
 				if (buffer_eq_slen(module, CONST_STR_LEN("mod_deflate")))
 					continue;
@@ -192,6 +190,8 @@ int plugins_load(server *srv) {
 				srv->srvconf.h2proto = 0;
 				continue;
 			}
+			log_perror(srv->errh, __FILE__, __LINE__,
+			  "LoadLibrary() %s", tb->ptr);
 			return -1;
 		}
 		buffer_copy_buffer(tb, module);
@@ -210,8 +210,6 @@ int plugins_load(server *srv) {
 		buffer_append_string_len(tb, CONST_STR_LEN(".so"));
 	   #endif
 		if (NULL == (lib = dlopen(tb->ptr, RTLD_NOW|RTLD_GLOBAL))) {
-			log_error(srv->errh, __FILE__, __LINE__,
-			  "dlopen() failed for: %s %s", tb->ptr, dlerror());
 			if (srv->srvconf.compat_module_load) {
 				if (buffer_eq_slen(module, CONST_STR_LEN("mod_deflate")))
 					continue;
@@ -220,6 +218,8 @@ int plugins_load(server *srv) {
 				srv->srvconf.h2proto = 0;
 				continue;
 			}
+			log_error(srv->errh, __FILE__, __LINE__,
+			  "dlopen() failed for: %s %s", tb->ptr, dlerror());
 			return -1;
 		}
 		buffer_clear(tb);
