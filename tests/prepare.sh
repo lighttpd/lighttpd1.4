15e0588ca4c02c0017a184c8d7b20954138b1b41
Glenn Strauss <gstrauss@gluelogic.com>
2024-01-08 10:07:32 -0500

    [tests] support platforms without cp -n
    (e.g. NetBSD and OpenBSD)

--- tests/prepare.sh
+++ tests/prepare.sh
@@ -36,11 +36,13 @@ cp "${srcdir}/docroot/"*.html \
 # copy configs to alternate build root, if alternate build root is used
 # (tests will fail to run from an alternate build root on platforms
 #  on which cp -n is not supported, such as NetBSD and OpenBSD)
-cp -n "${srcdir}/"*.conf \
+# (does not detect if srcdir or top_builddir are inconsistent with trailing '/')
+[[ "${srcdir}" = "." ]] || [[ "${srcdir}" = "${top_builddir}/tests" ]] || \
+cp    "${srcdir}/"*.conf \
       "${srcdir}/lighttpd.user" \
       "${srcdir}/lighttpd.htpasswd" \
       "${srcdir}/var-include-sub.conf" \
-      "${top_builddir}/tests/" 2>/dev/null || true
+      "${top_builddir}/tests/"
 
 # create some content
 touch "${tmpdir}/servers/www.example.org/pages/image.jpg" \
