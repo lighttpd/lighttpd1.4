218891528f549cb7f13b347d8b5b13603ed2a282
Glenn Strauss <gstrauss@gluelogic.com>
2024-01-09 20:27:12 -0500

    [tests] test_mod stub funcs for static builds
    For static builds, plugin.c contains references to module init funcs
    and if those modules are included in test_mod.c tests, then those
    symbols will be missing from test_mod, so create stubs for module
    init funcs, but rename to skip those included in test_mod.c tests.

--- src/t/test_mod.c
+++ src/t/test_mod.c
@@ -30,3 +30,37 @@ int main(void) {
 
     return 0;
 }
+
+
+#if defined(LIGHTTPD_STATIC)
+
+#include "base_decls.h" /*(plugin *)*/
+
+/* For static builds, plugin.c contains references to module init funcs
+ * and if those modules are included in test_mod.c tests, then those
+ * symbols will be missing from test_mod, so create stubs for module
+ * init funcs, but rename to skip those included in test_mod.c tests. */
+#define mod_access         mod_access_dup
+#define mod_alias          mod_alias_dup
+#define mod_evhost         mod_evhost_dup
+#define mod_expire         mod_expire_dup
+#define mod_indexfile      mod_indexfile_dup
+#define mod_simple_vhost   mod_simple_vhost_dup
+#define mod_ssi            mod_ssi_dup
+#define mod_staticfile     mod_staticfile_dup
+#define mod_userdir        mod_userdir_dup
+
+/* macro renaming above would not be needed with weak symbols,
+ * but this works on platforms without support for weak symbols */
+#define PLUGIN_INIT_EXPAND(x) \
+        int x ## _plugin_init(plugin *p); \
+        int x ## _plugin_init(plugin *p) { UNUSED(p); return 0; }
+#define PLUGIN_INIT(x) \
+        PLUGIN_INIT_EXPAND(x)
+
+#include "plugin-static.h"
+
+#undef PLUGIN_INIT
+#undef PLUGIN_INIT_EXPAND
+
+#endif /* LIGHTTPD_STATIC */
