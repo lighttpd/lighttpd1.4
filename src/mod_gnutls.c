lighttpd/lighttpd1.4

commit 9c266b805742a3cbf406bb7d44e2d8dd870383f3

[mod_gnutls] free kp if cert chain invalid
(coverity)

(This is related to commits just pushed; no impact to lighttpd releases)

src/mod_gnutls.c
@@ -1573,6 +1573,7 @@ network_gnutls_load_pemfile (server *srv, const buffer *pemfile, const buffer *p
     if (d->size > 1) { /*(certificate chain provided)*/
         int rc = mod_gnutls_construct_crt_chain(kp, d, srv->errh);
         if (rc < 0) {
+            mod_gnutls_kp_free(kp);
             mod_gnutls_free_config_crts(d);
             gnutls_privkey_deinit(pkey);
             free(pc);
@@ -1687,6 +1688,7 @@ mod_gnutls_acme_tls_1 (handler_ctx *hctx)
 
     rc = mod_gnutls_construct_crt_chain(kp, d, errh);
     if (rc < 0) {
+        mod_gnutls_kp_free(kp);
         mod_gnutls_free_config_crts(d);
         gnutls_privkey_deinit(pkey);
         return rc;

commit 9080f615a96df06228bc9044089c6fa4162afbbc

[mod_gnutls] avoid double-free in error path (unlikely)
avoid double-free in error path (unlikely)
  (lighttpd 1.4.78 - lighttpd 1.4.82)

mod_gnutls_construct_crt_chain() is unlikely to fail with
admin-generated certificates.

bug introduced in 9c266b805742a3cbf406bb7d44e2d8dd870383f3

thx Amit Pinchasi for report and write up

credit: Amit Pinchasi (https://linkedin.com/in/amit-pinchasi-2450a622a/)

src/mod_gnutls.c
@@ -1614,8 +1614,6 @@ network_gnutls_load_pemfile (server *srv, const buffer *pemfile, const buffer *p
         int rc = mod_gnutls_construct_crt_chain(kp, d, srv->errh);
         if (rc < 0) {
             mod_gnutls_kp_free(kp);
-            mod_gnutls_free_config_crts(d);
-            gnutls_privkey_deinit(pkey);
             free(pc);
             return NULL;
         }
@@ -1729,8 +1727,6 @@ mod_gnutls_acme_tls_1 (handler_ctx *hctx)
     rc = mod_gnutls_construct_crt_chain(kp, d, errh);
     if (rc < 0) {
         mod_gnutls_kp_free(kp);
-        mod_gnutls_free_config_crts(d);
-        gnutls_privkey_deinit(pkey);
         return rc;
     }
 