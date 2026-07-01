828cd0cdf1fa42f0ebc00e5a2a634c6905cafc9a
kali834x <kali834x@gmail.com>
2026-07-01 14:50:48 +0530

    [core] burl_normalize() query string offset parse
    burl_normalize() query string offset parsing of multiple '?'
    in underlying burl_normalize_basic_required()
    
    Co-authored-by: Glenn Strauss <gstrauss@gluelogic.com>
    
    x-ref:
      "[core] fix query string offset parsing in burl_normalize()"
      https://github.com/lighttpd/lighttpd1.4/pull/169
    
    github: closes #169

--- src/burl.c
+++ src/burl.c
@@ -144,7 +144,7 @@ static int burl_normalize_basic_required_fix (buffer *b, buffer *t, int i, int q
     for (; i < used; ++i, ++j) {
         if (!encoded_chars_http_uri_reqd[s[i]]) {
             p[j] = s[i];
-            if (__builtin_expect( (s[i] == '?'), 0)) qs = j;
+            if (__builtin_expect( (s[i] == '?'), 0) && -1 == qs) qs = j;
         }
         else if (s[i]=='%' && li_cton(s[i+1], n1) && li_cton(s[i+2], n2)) {
             const unsigned int x = (n1 << 4) | n2;
@@ -185,7 +185,7 @@ static int burl_normalize_basic_required (buffer *b, buffer *t)
 
     for (int i = 0; i < used; ++i) {
         if (!encoded_chars_http_uri_reqd[s[i]]) {
-            if (s[i] == '?') qs = i;
+            if (__builtin_expect( (s[i] == '?'), 0) && -1 == qs) qs = i;
         }
         else if (s[i]=='%' && li_cton(s[i+1], n1) && li_cton(s[i+2], n2)
                  && (encoded_chars_http_uri_reqd[(x = (n1 << 4) | n2)]
